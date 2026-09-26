30f44c6f089a75153ffb9ed71aa2c41059f5721f
kaori0205 <320223483+kaori0205@users.noreply.github.com>
2026-09-27 02:09:26 +0900

    [update]１１章６節ビューまで

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_25_162506) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_26_160628) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -47,11 +47,19 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_25_162506) do
     t.integer "user_id"
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "post_id"
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "location"
     t.string "text"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
